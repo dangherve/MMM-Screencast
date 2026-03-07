ce81e63432b7665df6f09e89db2d7572677a06f3
dangherve <dangherve@gmail.com>
2026-03-07 01:22:37 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/kevinatown/MMM-Screencast.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/MMM-Screencast.git
+git push --force --set-upstream origin master
